1d48ab00ceae9aa6a811f933287ce16de65bc618
lnb <921894@gmail.com>
2018-06-22 18:03:50 +0800

    day4

--- www/orm.py
+++ www/orm.py
@@ -9,6 +9,7 @@ import asyncio
 import logging
 from logging import log
 import aiomysql
+from _mysql_exceptions import StandardError
 
 @asyncio.coroutine
 def create_pool(loop,**kw):
@@ -55,7 +56,43 @@ def execute(sql,args):
             raise
         return affected
 
-class Model(dict,metaclass=ModelMetaclass):  # @UndefinedVariable
+class ModelMetaclass(type):
+    
+    def __new__(self,cls,name,bases,attrs):
+        if name=='Model':
+            return type.__new__(cls,name,bases,attrs)
+        tableName=attrs.get('__tables__',None) or name
+        logging.info('found model: %s (table: %s' % (name,tableName))
+        mappings=dict()
+        fields=[]
+        primaryKey=None
+        for k,v in attrs.items():
+            if isinstance(v,Field):
+                logging.info('  found mapping: %s ==> %s' % (k,v))
+                mappings[k]=vars()
+                if v.primary_key:
+                    #找到主键
+                    if primaryKey:
+                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    primaryKey=KeyboardInterrupt
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise StandardError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+            escaped_fields=list(map(lambda f: '`%s` % f, fields'))
+        attrs['__mappings__']=mappings #保存属性和列的映射关系
+        attrs['__table__']=tableName
+        attrs['__primary_key__']=primaryKey # 主键属性名
+        attrs['__fields__']=fields # 除主键外的属性名
+        attrs['__select__']='select `%s`,%s from `%s`' % (primaryKey,', '.join(escaped_fields),tableName)
+        attrs['__insert__']='insert into `%s` (%s, `%s`) values (%s)' % (tableName,', '.join(escaped_fields),primaryKey,create_args_string(len(escaped_fields)+1))
+        attrs['__update__']='update `%s` set %s where `%s`=?' % (tableName,', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f),fields)),primaryKey)
+        attrs['__delete__']='delete from `%s` where `%s`=?' % (tableName,primaryKey)
+        return type.__new__(cls,name,bases,attrs)
+
+class Model(dict,metaclass=ModelMetaclass):  
 
     def __init__(self,**kw):
         super(Model,self).__init__(**kw)
@@ -64,7 +101,7 @@ class Model(dict,metaclass=ModelMetaclass):  # @UndefinedVariable
         try:
             return self[key]
         except KeyError:
-            raise AttributeError(r"'Model' object has no attribute '%s'"%key)
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
 
     def __setattr__(self,key,value):
         self[key]=value
@@ -78,13 +115,13 @@ class Model(dict,metaclass=ModelMetaclass):  # @UndefinedVariable
             field=self.__mappings__[key]
             if field.default is not None:
                 value=field.default() if callable(field.default) else field.default
-                logging.debug('using default value for %s:%s'%(key,str(value)))
+                logging.debug('using default value for %s:%s' %(key,str(value)))
         return value
     @classmethod
     @asyncio.coroutine
     def find(cls,pk):
         ' find object by primary key.'
-        rs=yield from select ('%s where `%s`=?'%(cls.__select__,cls.__primary_key__),[pk],1)
+        rs=yield from select ('%s where `%s`=?' % (cls.__select__,cls.__primary_key__),[pk],1)
         if(len(rs)==0):
             return None
         return cls(**rs[0])
@@ -95,7 +132,7 @@ class Model(dict,metaclass=ModelMetaclass):  # @UndefinedVariable
         args.append(self.getValueOrDefault(self.__primary_key__))
         rows=yield from execute(self.__insert__,args)
         if rows!=1:
-            logging.warn('failed to insert record:affected rows:%s'% rows)
+            logging.warn('failed to insert record:affected rows:%s' % rows)
 
     @asyncio.coroutine
     def findAll(cls,where=None,args=None,**kw):  #  @NoSelf
@@ -120,18 +157,18 @@ class Model(dict,metaclass=ModelMetaclass):  # @UndefinedVariable
                 sql.append('?,?')
                 args.extend(limit)
             else:
-                raise ValueError('Invalid limit value:%s'%str(limit))
-        rs=await select(" ".join(sql), args)
+                raise ValueError('Invalid limit value:%s' % str(limit))
+        rs=yield from select(" ".join(sql), args)
         return [cls(**r) for r in rs]
 
     @asyncio.coroutine
     def findNumber(cls,selectField,where=None,args=None):  # @NoSelf
         ' find number by select and where.'
-        sql=['select %s _num_ from `%s`'%(selectField,cls.__table__)]
+        sql=['select %s _num_ from `%s`' % (selectField,cls.__table__)]
         if where:
             sql.append('where')
             sql.append(where)
-        rs=await select(' '.join(sql),args,1)  # @UndefinedVariable
+        rs=yield from select(' '.join(sql),args,1)  # @UndefinedVariable
         if len(rs)==0:
             return None
             return rs[0]['_num_']
@@ -140,15 +177,15 @@ class Model(dict,metaclass=ModelMetaclass):  # @UndefinedVariable
     def update(self):
         args=list(map(self.getValue,self.__fields__))
         args.append(self.getValue(self.__primary_key__))
-        rows=await execute(self.__update__,args)  # @UndefinedVariable
+        rows=yield from execute(self.__update__,args)  # @UndefinedVariable
         if rows!=1:
             logging.warn('failed to update by primary key:affected rows:%s'%rows)
     @asyncio.coroutine
     def remove(self):
         args=[self.getValue(self.__primary_key__)]
-        rows=await execute(self__delete__,args)  # @UndefinedVariable
+        rows=yield from execute(self__delete__,args)  # @UndefinedVariable
         if rows!=1:
-            logging.warn('failed to remove by primary key: affected rows:%s'%rows)
+            logging.warn('failed to remove by primary key: affected rows:%s' % rows)
 
 class Field(object):
 
@@ -159,7 +196,7 @@ class Field(object):
         self.default=default
 
     def __str__(self):
-        return '<%s,%s:%s>'%(self.__class__.__name__,self.column_type,self.name)
+        return '<%s,%s:%s>' % (self.__class__.__name__,self.column_type,self.name)
 
 class StringField(Field):
 
@@ -184,4 +221,11 @@ class TextField(Field):
 
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
+def create_args_string(num):
+    L=[]
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+    
 
+        
--- www/test.py
+++ www/test.py
@@ -4,13 +4,13 @@ Created on 2018��6��21��
 
 @author: taihao
 '''
-import orm
-from models import User, Blog, Comment
+from www import orm
 import asyncio
+from www.models import User
 
 def test():
-    yield from orm.create_pool(user='root',password='123456',database='awesome')
-    u=User(name='Test',email='test@example.com',passwd='12343333',image='about:blank')
+    yield from orm.create_pool(user='root',password='123456',database='awesome', host='127.0.0.1',)
+    u=User(name='Test',email='test@example.com',passwd='12343333',image='about:blank',id="100")
     yield from u.save()
     
 loop = asyncio.get_event_loop()
