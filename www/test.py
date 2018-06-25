a76d53fbd84c3bb39be21f603d02c1ed0438fe5a
lnb <921894@gmail.com>
2018-06-25 18:11:07 +0800

    day04modified

--- www/test.py
+++ www/test.py
@@ -1,17 +1,18 @@
 # -*- coding: UTF-8 -*-
 '''
-Created on 2018��6��21��
+Created on 2018锟斤拷6锟斤拷21锟斤拷
 
 @author: taihao
 '''
 import asyncio
 from www.models import User
 from www import orm
-async def test():
-    await orm.create_pool(user='root',password='123456',database='awesome')
-    u=User(name='Test',email='test@example.com',passwd='12343333',image='about:blank',id="100")
+from aiomysql import pool
+async def test(loop):
+    await orm.create_pool(loop,user='root',password='123456',db='awesome')
+    u=User(name='Test',email='test1@example.com',passwd='12343333',image='about:blank',id="101")
     await u.save()
     
 loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.wait(test()))
+loop.run_until_complete(test(loop))
 loop.close()    
